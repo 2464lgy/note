e31ab98cad311144eee87ce3077956a62048b549
niejieqiong <niejieqiong@jiagouyun.com>
2019-06-14 15:28:51 +0800

    NodeJS
    最近在学习NodeJS呢

--- React/路由/App.js
+++ React/路由/App.js
@@ -60,3 +60,106 @@ class App extends React.Component {
 
 export default App;
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $scope.valueScope=[
+    {
+      max:255,
+      min:0,
+    },
+    {
+      max:255,
+      min:0,
+    },
+    {
+      max:255,
+      min:0,
+    },
+    {
+      max:255,
+      min:0,
+    }
+  ]
+  const CidrCheck=(CidrBlock)=>{
+    let strArr=CidrBlock.split('/');
+    let num=0;
+    if(strArr[1]<8){
+       num=strArr[1];
+    }else{
+      num=strArr[1]-0+parseInt(strArr[1]/8)-1;
+    }
+    let strArr2= strArr[0].split('.').map(item=>{
+      return ('00000000'+(Number(item)).toString(2)).substr(-8.8); //转换为二进制数且不足八位补零
+    })
+    console.log(strArr2);
+    console.log(num);
+    console.log(strArr2.join('/').substr(0,num))
+    let fixedValue=strArr2.join('/').substr(0,num).split('/');//这里是固定的前几位
+    let length=fixedValue.length;
+    let  valueScope=fixedValue.map((item,index)=>{
+      if(index<length-1){
+        let max=parseInt((item+'0000000').substr(0,8),2);
+        return {
+          max:max,
+          min:max,
+        } 
+      }else{
+        let max=parseInt((item+'0000000').substr(0,8),2);
+        return {
+          max:max,
+          min:0,
+        }
+      }      
+    })
+    if(valueScope.length<4){
+      for(let i=5;i>valueScope.length;i--){
+        valueScope.push(
+          {
+            max:255,
+            min:0,
+          }
+        )
+      }
+    }
+    $scope.valueScope=valueScope;
+    console.log($scope.valueScope)
+  }
+
+
+
+
+
+
+
+
+
+        <div class="col-sm-2">
+        <input type="text" class="form-control" ng-model="modal.first" ng-disabled="modal.IPType==='fromEcs'" ng-pattern="/^\d{1,3}$/" ui-validate="{a:'$value<=255',b:'$value>=1'}" required>
+        <input type="text" class="form-control" ng-model="modal.first" ng-disabled="modal.IPType==='fromEcs'"   ui-validate="{{+valueScope[0].max}}?{a:'$value<= {{+valueScope[0].max}}',b:'$value>={{+valueScope[0].min}}'}:{}"  required>
+      </div>
+      <div class="col-sm-2">
+        <input type="text" class="form-control" ng-model="modal.second" ng-disabled="modal.IPType==='fromEcs'" ng-pattern="/^\d{1,3}$/" ui-validate="{a:'$value<=255',b:'$value>=0'}" required>
+        <input type="text" class="form-control" ng-model="modal.second" ng-disabled="modal.IPType==='fromEcs'" ng-pattern="/^\d{1,3}$/" ui-validate="{a:'$value<={{valueScope[1].max}}',b:'$value>={{valueScope[1].min}}'}" required>
+      </div>
+      <div class="col-sm-2">
+        <input type="text" class="form-control" ng-model="modal.third" ng-disabled="modal.IPType==='fromEcs'" ng-pattern="/^\d{1,3}$/" ui-validate="{a:'$value<=255',b:'$value>=0'}" required>
+        <input type="text" class="form-control" ng-model="modal.third" ng-disabled="modal.IPType==='fromEcs'" ng-pattern="/^\d{1,3}$/"  ui-validate="{a:'$value<={{valueScope[2].max}}',b:'$value>={{valueScope[2].min}}'}" required>
+      </div>
+      <div class="col-sm-2">
+        <input type="text" class="form-control" ng-model="modal.forth" ng-disabled="modal.IPType==='fromEcs'" ng-pattern="/^\d{1,3}$/" ui-validate="{a:'$value<=255',b:'$value>=0'}" required>
+        <input type="text" class="form-control" ng-model="modal.forth" ng-disabled="modal.IPType==='fromEcs'" ng-pattern="/^\d{1,3}$/"  ui-validate="{a:'$value<={{valueScope[1].max}}',b:'$value>={{valueScope[1].min}}'}" required>
+      </div>
